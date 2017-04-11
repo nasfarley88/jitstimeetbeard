7a1096f8ffdbdbd6f70e012d968a3e4a981ad77f
Nathanael Farley <nasfarley88@gmail.com>
2017-04-11 14:14:46 +0100

    Committed code from jitsistach.py.

--- python/jitsimeetbeard/__init__.py
+++ python/jitsimeetbeard/__init__.py
@@ -1,23 +1,43 @@
+import random
+from functools import lru_cache
+from urllib.request import urlopen
+
 from skybeard.beards import BeardChatHandler
 from skybeard.decorators import onerror
-from skybeard.utils import get_args
 
 
-class Jitsimeetbeard(BeardChatHandler):
+class JitsiMeetBeard(BeardChatHandler):
 
-    __userhelp__ = """Default help message."""
+    __userhelp__ = "Makes jitsi meet room per telegram chat."
 
     __commands__ = [
-        # command, callback coro, help text
-        ("echo", 'echo', 'Echos arguments. e.g. <code>/echo [arg1 [arg2 ... ]]</code>')
+        ("videoroom", 'jitsi_meet', 'Sends link for video chat')
     ]
 
     # __init__ is implicit
 
+    @lru_cache()
+    def _get_words(self):
+        # Open list of words from:
+        # https://github.com/first20hours/google-10000-english
+        words_file = urlopen("https://raw.githubusercontent.com/first20hours"
+                             "/google-10000-english/master/google-10000-"
+                             "english-usa-no-swears-medium.txt")
+        words = words_file.read().decode('utf-8').split("\n")
+        words.remove('')
+
+        words = [x.capitalize() for x in words]
+        return words
+
     @onerror
-    async def echo(self, msg):
-        args = get_args(msg)
-        if args:
-            await self.sender.sendMessage("Args: {}".format(args))
-        else:
-            await self.sender.sendMessage("No arguments given.")
+    async def jitsi_meet(self, msg):
+        chat_id = msg['chat']['id']
+        random.seed(chat_id)
+
+        room_name = "".join(
+            (random.choice(self._get_words()) for x in range(5)))
+        link = "https://meet.jit.si/"+room_name
+
+        await self.sender.sendMessage(
+            "Video room (click to join): {l}\n\nJitsi room name: \n\n*{r}*\n\n(On mobile, you may need to install the app)".format(r=room_name, l=link),
+            parse_mode='Markdown')
